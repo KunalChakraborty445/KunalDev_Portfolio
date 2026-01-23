0550a8d0c37fa7663899de196455bfdf7fbcef89
Kunal <kunalchakr32@gmail.com>
2026-01-23 15:33:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from "#componets/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <Navbar />
+        </main>
+    )
+}
+export default App
